8a23aaa84129ae876f6097b216f92fbc548648b7
Icvara Barbier <ibarbier@login.curnagl>
2021-07-02 16:26:09 +0200

    Tuned sbatch for Icvara

---
+++ sbatch_icvara.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --time 00-00:15:00
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 12
+#SBATCH --array 1-8
+
+
+env | grep SLURM
+
+module load gcc python
+
+DIR=/users/ibarbier
+source $DIR/myfirstvenv/bin/activate
+
+
+IN=$(sed -n ${SLURM_ARRAY_TASK_ID}p data/input.txt)
+echo IN = $IN
+
+
+python3 abc_smc.py $IN $SLURM_CPUS_PER_TASK
+
+deactivate
