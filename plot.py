846a065beb13473748ce27734bf0918c93e785e5
icvara <icvara.barbier@gmail.com>
2021-07-02 23:54:10 +0100

    improve plot funtion

--- plot.py
+++ plot.py
@@ -6,34 +6,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import abc_smc
 
-sample="sg1"
 number="final" 
 
-#raw_output= np.loadtxt('smc\pars_final.out')
-path = 'smc_'+sample+'\pars_' + number + '.out'
-raw_output= np.loadtxt(path)
+for sample in ['sg1','sg2','sg3','sg4','sg5','sg6','sg1t4','sg4t4']:
+    path = 'smc_'+sample+'\pars_' + number + '.out'
+    raw_output= np.loadtxt(path)
+    df = pd.DataFrame(raw_output, columns = ['Finf','xc','n','L1','B1','log_k1','n1'])
+    p=np.mean(df)
+    print(p)
+    
+    path='data/data.txt'
+    dataframe = pd.read_csv(path,sep='\t')
+    x_data,y_data,max_input= abc_smc.Get_data(dataframe,sample)
+    x=np.copy(x_data)
+    x[0]=1e-8
 
-#df = pd.DataFrame(raw_output, columns = ['F0','Finf','xc','n'])
-df = pd.DataFrame(raw_output, columns = ['Finf','xc','n','L1','B1','log_k1','n1'])
+   # for index, p in dfmean.iterrows():
+    y_model=abc_smc.model(x,max_input,p)
+   # plt.plot(x_data, y_model,'-r')
+    plt.close()
+    plt.plot(x, y_model,'-r',label="models")
+    plt.plot(x,y_data,'bo',label=sample)
+    plt.xscale('log')
+    plt.xlabel("arabinose [%]")
+    plt.ylabel("Normalized GFP")
+    plt.legend()
+    plt.xscale('log')
+    labels=x_data
+    plt.xticks(x, labels, rotation='vertical')
+    #plt.show()
+    plt.savefig(sample+'data+fit.pdf', bbox_inches='tight')
 
-#distance= np.loadtxt('smc\distances_final.out')
-#distance= np.loadtxt('smc\distances_17.out')
-#df['distance']=distance
-#df_2= df[df["distance"] < 0.05]
+    plt.close()
+    sns.pairplot(df)
+    plt.savefig(sample+'par_plot.pdf', bbox_inches='tight')
 
-x_data=np.array([0, 3.125e-06, 6.250e-06, 1.250e-05, 2.500e-05, 5.000e-05,
-                 1.000e-04, 2.000e-04, 2.000e-01])
-y_data = np.array([ 541388.61, 499462.04, 450763.51, 295709.72,
-                   141880.68,  69208.92, 39025.37,  29463.05,  12465.16])
-y_data = y_data/np.max(y_data) #fix the max to 1
-
-#y_model=p['Finf']+(p['F0']-p['Finf'])/(1+np.power(x/p['xc'],p['n']))
 
 '''
+path='data/data.txt'
+dataframe = pd.read_csv(path,sep='\t')
+dataframe['arabinose']=dataframe['arabinose'].astype(str)
+
+x_data,y_data,max_input= abc_smc.Get_data(dataframe,sample)
+
+
+
 for index, p in df1.iterrows():
     y_model=p['Finf']+(p['F0']-p['Finf'])/(1+np.power(x_data/p['xc'],p['n']))
     plt.plot(x_data, y_model,'--g')
-'''
+
 dtp=df
 
 plt.title("")
@@ -41,7 +62,7 @@ x=np.copy(x_data)
 x[0]=1e-8
 
 for index, p in dtp.iterrows():
-    y_model=abc_smc.model(x,p)
+    y_model=abc_smc.model(x,max_input,p)
     plt.plot(x, y_model,'-r')
 
 plt.plot(x, y_model,'-r',label="models")
@@ -61,7 +82,7 @@ plt.savefig(sample+'model_par_abc_smc.pdf', bbox_inches='tight')
 sns.pairplot(dtp)
 plt.savefig(sample+'par_plot.pdf', bbox_inches='tight')
 
-'''
+
 
 plt.title("")
 plt.xlabel("arabinose [%]")
@@ -79,4 +100,6 @@ plt.savefig('model_par_manuel.pdf', bbox_inches='tight')
 
 
 plt.show()
+
 '''
+print('youpi')
