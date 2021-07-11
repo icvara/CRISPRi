3d923299c7aa02117644c92d141a233d4cd9fd1c
icvara <icvara.barbier@gmail.com>
2021-07-11 14:48:14 +0100

    add final par data generation

--- plot.py
+++ plot.py
@@ -9,6 +9,8 @@ import statistics
 
 from collections import Counter
 
+final_par = pd.DataFrame(columns=['Finf','xc','n','L1','B1','log_k1','n1','distance'], index= ['sg1','sg2','sg3','sg4','sg5','sg6','sg1t4','sg4t4'])
+
 number="final"
 
 
@@ -26,16 +28,16 @@ for sample in ['sg1','sg2','sg3','sg4','sg5','sg6','sg1t4','sg4t4']:
 
     p_min=df[df['dist']==min(df['dist'])]
 
+    
+
     p= [ p_min['Finf'].tolist()[0], p_min['xc'].tolist()[0], p_min['n'].tolist()[0],
           p_min['L1'].tolist()[0], p_min['B1'].tolist()[0], p_min['log_k1'].tolist()[0],
           p_min['n1'].tolist()[0] ]
+    p2=p.copy()
+    p2.append(min(df['dist']))
+    final_par.loc[sample]=p2
+    
 
-    '''
-
-    p= [Counter(df['Finf']).most_common()[0][0],Counter(df['xc']).most_common()[0][0],Counter(df['n']).most_common()[0][0],
-        Counter(df['L1']).most_common()[0][0],Counter(df['B1']).most_common()[0][0],Counter(df['log_k1']).most_common()[0][0],
-        Counter(df['n1']).most_common()[0][0]]
-    '''
     path='data/data.txt'
     dataframe = pd.read_csv(path,sep='\t')
     x_zero=1e-8
@@ -51,9 +53,9 @@ for sample in ['sg1','sg2','sg3','sg4','sg5','sg6','sg1t4','sg4t4']:
 
     x[0] = x2[0] = x_zero
 
-    plt.close()
-    plt.plot(x2, y_model,'-r',label="models")
-    plt.plot(x,y_data,'bo',label=sample)
+    plt.close() #
+    plt.plot(x2, y_model,'-',label="models")
+    plt.plot(x,y_data,'o',label=sample)
     plt.xscale('log')
     plt.xlabel("arabinose [%]")
     plt.ylabel("Normalized GFP")
@@ -62,22 +64,14 @@ for sample in ['sg1','sg2','sg3','sg4','sg5','sg6','sg1t4','sg4t4']:
     labels=x_data
     plt.xticks(x, labels, rotation='vertical')
     #plt.show()
-    plt.savefig(sample+'data+fit.pdf', bbox_inches='tight')
+    plt.savefig(sample+'data+fit.pdf', bbox_inches='tight') #
 
     plt.close()
 
     g=sns.pairplot(df[['Finf','xc','n','L1','B1','log_k1','n1']], kind='kde')
-    '''
-    g.axes[0,0].set_xlim((abc_smc.parlist[0]['lower_limit'],abc_smc.parlist[0]['upper_limit']))
-    g.axes[1,1].set_xlim((abc_smc.parlist[1]['lower_limit'],abc_smc.parlist[1]['upper_limit']))
-    g.axes[2,2].set_xlim((abc_smc.parlist[2]['lower_limit'],abc_smc.parlist[2]['upper_limit']))
-    g.axes[3,3].set_xlim((abc_smc.parlist[3]['lower_limit'],abc_smc.parlist[3]['upper_limit']))
-    g.axes[4,4].set_xlim((abc_smc.parlist[4]['lower_limit'],abc_smc.parlist[4]['upper_limit']))
-    g.axes[5,5].set_xlim((abc_smc.parlist[5]['lower_limit'],abc_smc.parlist[5]['upper_limit']))
-    g.axes[6,6].set_xlim((abc_smc.parlist[6]['lower_limit'],abc_smc.parlist[6]['upper_limit']))
-    '''
     #plt.show()
     plt.savefig(sample+'par_plot.pdf', bbox_inches='tight')
     print(sample)
 
-
+#plt.savefig('ALL_data+fit.pdf', bbox_inches='tight')
+final_par.to_csv('used_par.csv')
